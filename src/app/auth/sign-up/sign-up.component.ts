25e0c00deaa30037745c9d49352ab02f486f1732
hmaravlad <hmara.vladislav2@gmail.com>
2022-05-16 00:03:04 +0300

    add visual part of sign up form

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,6 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder, 
+    private userService: UserService, 
+    private location: Location,
+  ) {}
+
+  signUpForm = this.fb.group({
+    email: this.fb.control(['']),
+    password: this.fb.control(['']),
+    confirmPassword: this.fb.control(['']),
+  });
+
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
   }
 }
