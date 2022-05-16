3169a7a3d0036e143f3e0e5cdd01e216e2600ccf
hmaravlad <hmara.vladislav2@gmail.com>
2022-05-16 19:38:09 +0300

    add sign in functionality

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,6 +1,8 @@
-import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { isErrorResponse } from '../query-error.entity';
+import { isUser } from '../user.entity';
 import { UserService } from '../user.service';
 
 @Component({
@@ -12,8 +14,10 @@ export class SignInComponent implements OnInit {
   constructor(
     private fb: FormBuilder, 
     private userService: UserService, 
-    private location: Location,
+    private router: Router,
   ) {}
+
+  error = '';
 
   signInForm = this.fb.group({
     email: this.fb.control(['']),
@@ -24,5 +28,13 @@ export class SignInComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.userService.login(this.signInForm.value).subscribe(x => {
+      if (isUser(x)) {
+        this.error = '';
+        this.router.navigateByUrl('/projects');
+      } else if (isErrorResponse(x)) {
+        this.error = x.message[0];
+      }
+    });
   }
 }
