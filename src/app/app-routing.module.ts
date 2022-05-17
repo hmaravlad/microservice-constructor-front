070b71c625fcec25d0ae41099ac1b5c1300e28ca
hmaravlad <hmara.vladislav2@gmail.com>
2022-05-17 14:37:12 +0300

    add workspace module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
   { path: 'projects', component: ProjectsComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'workspace', component: WorkspaceComponent },
 ];
 
 @NgModule({
---
+++ src/app/workspace/workspace.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkspaceComponent } from './workspace.component';
+
+
+
+@NgModule({
+  declarations: [
+    WorkspaceComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WorkspaceModule { }
