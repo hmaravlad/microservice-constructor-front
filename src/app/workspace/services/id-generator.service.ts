ea44b75fcfcc64c9231c0943f0b0f7c2eaa5df84
hmaravlad <hmara.vladislav2@gmail.com>
2022-05-30 18:58:00 +0300

    add setting max id for id-generator-service

--- src/app/workspace/services/id-generator.service.ts
+++ src/app/workspace/services/id-generator.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class IdGeneratorService {
   counter = 0;
 
   getId(): number {
     this.counter += 1;
     return this.counter;
+  }
+
+  trySetMaxId(id: number) {
+    if (id > this.counter) {
+      this.counter = id + 1;
+    }
   }
 }
